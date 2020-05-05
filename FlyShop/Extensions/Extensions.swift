260631c263d62058e0f1fe53177208c2e8ff38f6
KALIMI <karenmirakyan@gmail.com>
2020-05-05 16:57:04 +0400

    first commit

---
+++ FlyShop/Extensions/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  FlyShop
+//
+//  Created by Karen Mirakyan on 5/5/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import Foundation
---
+++ FlyShop/Views/SignUp.swift
@@ -0,0 +1,21 @@
+//
+//  SignUp.swift
+//  FlyShop
+//
+//  Created by Karen Mirakyan on 5/5/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
+}
