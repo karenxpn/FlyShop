44acf1d6743825f29a918d9e36b60972c9684151
KALIMI <karenmirakyan@gmail.com>
2020-05-05 13:53:43 +0400

    Initial Commit

---
+++ FlyShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlyShop
+//
+//  Created by Karen Mirakyan on 5/5/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
