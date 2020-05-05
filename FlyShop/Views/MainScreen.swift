0102cc80d296ff5fde82271cca7fa28449ff3093
KALIMI <karenmirakyan@gmail.com>
2020-05-05 16:57:04 +0400

    first commit

---
+++ FlyShop/Views/MainScreen.swift
@@ -0,0 +1,21 @@
+//
+//  MainScreen.swift
+//  FlyShop
+//
+//  Created by Karen Mirakyan on 5/5/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
+}
