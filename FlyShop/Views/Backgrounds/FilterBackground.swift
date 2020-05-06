07b3f2a75766ee066089e09063fc021b4168ffca
KALIMI <karenmirakyan@gmail.com>
2020-05-06 16:08:25 +0400

    some changes done on filter part

---
+++ FlyShop/Views/Backgrounds/FilterBackground.swift
@@ -0,0 +1,18 @@
+//
+//  FilterBackground.swift
+//  FlyShop
+//
+//  Created by Karen Mirakyan on 5/6/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct FilterBackground: View {
+    var body: some View {
+        Image( "filterBackground" )
+        .resizable()
+        .edgesIgnoringSafeArea(.all)
+    }
+}
