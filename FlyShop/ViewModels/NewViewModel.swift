d69d0d961c24953200347a580fae595cee862402
KALIMI <karenmirakyan@gmail.com>
2020-05-07 19:15:39 +0400

    New screen filter done

--- FlyShop/ViewModels/NewViewModel.swift
+++ FlyShop/ViewModels/NewViewModel.swift
@@ -18,10 +18,24 @@ class NewViewModel: ObservableObject {
         NewService().getNew { (result) in
             NewService().getNew { (result) in
                 if let result = result {
-                    self.newItemList = result.map( ProductViewModel.init )
+                    self.newItemList = result.filter{self.convertToDate(startDate: $0.date) <= 30}.map( ProductViewModel.init )
                 }
             }
         }
+    }
+    
+    func convertToDate( startDate: String ) -> Int {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "hy")
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        let formatedStartDate = dateFormatter.date(from: startDate)
+        let currentDate = Date()
+        let components = Set<Calendar.Component>([.day])
+        let differenceOfDate = Calendar.current.dateComponents(components, from: formatedStartDate!, to: currentDate)
+
+        return differenceOfDate.day!
+
     }
     
 }
