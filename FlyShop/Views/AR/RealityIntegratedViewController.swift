7450dbf27c97144ef8ddfab10482d36398298158
KALIMI <karenmirakyan@gmail.com>
2020-06-02 23:54:10 +0400

    Ar View added to selected product

---
+++ FlyShop/Views/AR/RealityIntegratedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RealityIntegratedViewController.swift
+//  FlyShop
+//
+//  Created by Karen Mirakyan on 6/1/20.
+//  Copyright © 2020 Karen Mirakyan. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct RealityIntegratedViewController: UIViewControllerRepresentable {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<RealityIntegratedViewController>) -> RealityIntegratedViewController.UIViewControllerType {
+        return RealityViewController()
+    }
+    func updateUIViewController(_ uiViewController: RealityViewController, context: UIViewControllerRepresentableContext<RealityIntegratedViewController>) {
+    }
+}
